1411beb473c723a9337c57156ede754b3b41d406
Sepandi <56546033+Sepandi@users.noreply.github.com>
2021-10-22 11:53:26 +0330

    Number Guesser Game

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace NumberGueser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool PlayAgain = true;
+            int minNum = 1;
+            int maxNum = 10;
+            int input;
+            int number;
+            int countOfguesses;
+            string response;
+            Console.WriteLine("Guesing Number Game by Sepandi:D");
+            Console.WriteLine();
+            Console.WriteLine();
+            while (PlayAgain == true )
+            {
+                input = 0;
+                countOfguesses = 0;
+                number = random.Next(minNum, maxNum+1);
+                while(input != number)
+                {
+                    Console.WriteLine("Guese a Number Between "+ minNum + "-" + maxNum);
+                    input = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+
+                    if (input > number)
+                    {
+                        Console.WriteLine(input + " is to High.");
+                    }
+                    else if (input < number)
+                    {
+                        Console.WriteLine(input + " is to Low.");
+                    }
+                    countOfguesses += 1;
+                }
+                Console.WriteLine("Number was " + number + ". You Got it . Good Job. "+"Your Guese Count : " + countOfguesses);
+                Console.Write("Play Again ? (Y/N): ");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if (response == "Y")
+                {
+                    PlayAgain = true;
+
+                }
+                else
+                {
+                    PlayAgain = false;
+                }
+            }
+        }
+    }
+}
